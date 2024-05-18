18a3cdff6fcd99f68fc414b58864980389b71dce
Krzysztof Jagoda <krzysztof.jagoda99@gmail.com>
2024-05-18 16:27:39 +0200

    Implemented routing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { AsyncPipe, NgIf, NgOptimizedImage } from '@angular/common';
 import { TimerService } from './timer.service';
 import { FetchRandomUserService } from './fetch-random-user.service';
@@ -7,7 +7,7 @@ import { FetchRandomUserService } from './fetch-random-user.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsyncPipe, NgIf, NgOptimizedImage],
+  imports: [RouterOutlet, AsyncPipe, NgIf, NgOptimizedImage, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
